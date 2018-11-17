ed253dc4294fa6c66b7d79719b7d839f6cab93d7
Jerard Carney <jercarne@uat.edu>
2018-11-17 16:48:21 -0700

    First attempt at a game>

---
+++ Milestone_ONE/Assets/scripts/playerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+   
+    private Vector3 mousePosition;
+    public float moveSpeed = 0.1f;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+
+        if (Input.GetMouseButton(1) || (Input.GetKey(KeyCode.UpArrow)))
+        {
+            mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            transform.position = Vector2.Lerp(transform.position, mousePosition, moveSpeed);
+        }
+    }
+}
